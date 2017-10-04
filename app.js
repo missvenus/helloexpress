d1425b6dcb06624b2059d93d7455d3be692106d4
missvenus <missvenus.jain@gmail.com>
2017-10-04 16:57:02 +0100

    Added middleware support sass, jquery

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+ var $ = require('jquery');
 var express = require('express');
+var exphbs  = require('express-handlebars');
+var sassMiddleware = require('node-sass-middleware');
+var browserify = require('browserify-middleware');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,9 +16,10 @@ var todos = require('./routes/todos');
 
 var app = express();
 
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+ app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
+ app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,7 +32,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/todos', todos); 
+app.use (
+   sassMiddleware({
+     src: __dirname + '/sass',
+     dest: __dirname + '/public',
+     debug: true,
+   })
+ );
 
+app.get('/javascripts/bundle.js', browserify('./client/script.js'));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -44,5 +58,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
---
+++ client/script.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//   var checkboxes = document.getElementsByTagName('input');
+//   for (var i = 0; i < checkboxes.length; i++) {
+//     checkboxes[i].addEventListener('click', clickHandler);
+//   }
+// });
+
+// function clickHandler() {
+//   if(this.checked) {
+//     this.parentNode.className = 'checked';
+//   } else {
+//     this.parentNode.className = '';
+//   }
+// }
+
+$(function() {
+  $('input').on('click', function() {
+    $(this).parent().toggleClass('checked');
+  });
+});
